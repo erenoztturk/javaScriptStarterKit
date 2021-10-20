57d606a6ef269de0be9209c0b3cbb17a47b7e1e9
EREN ÖZTÜRK <erenozturk5772@gmail.com>
2021-10-20 20:25:28 +0300

    18 Ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log("mehaba kodlamaio")
+
+
+
+let konutKredileri = ["konut kredisi", "emlak konut kredisi", "kamu konut kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i <konutKredileri.length ; i++) {
+    console.log("<li>" +konutKredileri[i]+ "</li>");
+    
+}
+console.log("</ul>")
